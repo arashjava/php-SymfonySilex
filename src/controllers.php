168f646c3e1f4fbae2e9154ebc70164153ad4395
Arash Shadmani <shadmaniarash@gmail.com>
2018-02-19 11:21:15 -0500

    first commit

--- src/controllers.php
+++ src/controllers.php
@@ -6,7 +6,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
-
+    // set the per page variable global
+    $twig->addGlobal('perPage', $app['session']->get('perPage'));
+    // set the last page for pagination variable global
+    $twig->addGlobal('lastPage', $app['session']->get('lastPage'));
+    // set the current page for pagination variable global
+    $twig->addGlobal('curPage', $app['session']->get('curPage'));
     return $twig;
 }));
 
@@ -35,18 +40,25 @@ $app->match('/login', function (Request $request) use ($app) {
     return $app['twig']->render('login.html', array());
 });
 
+// save record per page for Todos in Session
+$app->match('/todo/perpage/{perPage}', function ($perPage)  use ($app) {
+    if ($perPage>0){
+        $app['session']->set('perPage', $perPage);
+        return $app->redirect('/todo');
+    }
+ });
 
 $app->get('/logout', function () use ($app) {
-    $app['session']->set('user', null);
+ //   $app['session']->set('user', null);
+    $app['session']->clear();
     return $app->redirect('/');
 });
 
 
-$app->get('/todo/{id}', function ($id) use ($app) {
+$app->get('/todo/record/{id}', function ($id) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
-
     if ($id){
         $sql = "SELECT * FROM todos WHERE id = '$id'";
         $todo = $app['db']->fetchAssoc($sql);
@@ -54,16 +66,42 @@ $app->get('/todo/{id}', function ($id) use ($app) {
             'todo' => $todo,
             ]);
     }
-    else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $app['db']->fetchAll($sql);
-
-        return $app['twig']->render('todos.html', [
-            'todos' => $todos,
-        ]);
-    }
 })
 ->value('id', null);
+
+
+$app->get('/todo/{page}', function ($page) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+    //  set number of rows per page
+    $perPage=(int)$app['session']->get('perPage');
+    if ($perPage<1) $perPage=4;     // considers default row numbers per page = 4
+    
+    // set page =1 as default
+    if (!$page) $page=1;
+    
+     $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
+    // get and set number of records for pagination
+    $num_rows = $app['db']->executeQuery($sql)->rowCount();
+    $lastPage = ceil($num_rows/$perPage);
+    $app['session']->set('lastPage', $lastPage);
+    //  save the current page for pagination in session
+    if ($page >0) $app['session']->set('curPage', $page);
+    
+    // record to start with
+    $start = ($page>1) ?($page *$perPage)-$perPage:0;   
+    //   pagination with using $start, $perPage variables
+    $sql = $sql. " Limit $start, $perPage";
+    $todos = $app['db']->fetchAll($sql);
+
+    return $app['twig']->render('todos.html', [
+        'todos' => $todos,
+    ]);
+})
+->value('page', null);
+
+
 
 //  Show the Todo in JSON format
 $app->get('/todo/json/{id}', function ($id, $json=NULL) use ($app) {
@@ -82,10 +120,9 @@ $app->post('/todo/add', function (Request $request) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
-
-    
     $user_id = $user['id'];
     $description = $request->get('description');
+
     // a user can't add a todo without a description
     if ($description !=NULL){
         $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
@@ -97,8 +134,9 @@ $app->post('/todo/add', function (Request $request) use ($app) {
         // set failure flash messages
         $app['session']->getFlashBag()->add('warning', 'Record is not added!');
     }
-
-    return $app->redirect('/todo');
+     //  to keep the curent page the same when program gets back to user
+    $curPage = $app['session']->get('curPage');
+    return $app->redirect('/todo/'.$curPage);
 });
 
 
@@ -107,14 +145,19 @@ $app->match('/todo/delete/{id}', function ($id) use ($app) {
     $sql = "DELETE FROM todos WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
     $app['session']->getFlashBag()->add('success', 'Record is deleted successfully');
-    return $app->redirect('/todo');
-});
+     //  to keep the curent page the same when program gets back to user
+    $curPage = $app['session']->get('curPage');
+    return $app->redirect('/todo/'.$curPage);
+})
+->value('id', null);
 
 // Confirmation that the task is completed / or change it back to not completed
 $app->match('/todo/completed/{id}', function ($id) use ($app) {
 
     $sql = "update todos set completed = !completed WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
-
-    return $app->redirect('/todo');
-});
+    //  to keep the curent page the same when program gets back to user
+    $curPage = $app['session']->get('curPage');
+    return $app->redirect('/todo/'.$curPage);
+})
+->value('id', null);
--- web/index.php
+++ web/index.php
@@ -1,17 +1,64 @@
 <?php
 
-use Symfony\Component\Debug\Debug;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
+use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 
-require_once __DIR__.'/../vendor/autoload.php';
+$baseDir = __DIR__ . '/../';
 
-Debug::enable();
+$loader = require $baseDir . '/vendor/autoload.php';
 
-$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
-if (php_sapi_name() === 'cli-server' && is_file($filename)) {
-    return false;
-}
+$app = new Application();
 
-$app = require __DIR__.'/../src/app.php';
-require __DIR__.'/../config/dev.php';
-require __DIR__.'/../src/controllers.php';
+$app->error(function (Exception $e) use ($app) {
+    return new \Symfony\Component\HttpFoundation\Response("Something goes terribly wrong: " . $e->getMessage());
+});
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+$app->register(
+    new DoctrineServiceProvider(),
+    [
+        'db.options' => [
+            'driver'        => 'pdo_mysql',
+            'host'          => 'localhost',
+            'dbname'        => 'sample',
+            'user'          => 'root',
+            'password'      => 'baaykdazyg',
+            'charset'       => 'utf8',
+            'driverOptions' => [
+                1002 => 'SET NAMES utf8',
+            ],
+        ],
+    ]
+);
+
+$app->register(new DoctrineOrmServiceProvider(), [
+    'orm.proxies_dir'             => $baseDir . 'src/App/Entity/Proxy',
+    'orm.auto_generate_proxies'   => $app['debug'],
+    'orm.em.options'              => [
+        'mappings' => [
+            [
+                'type'                         => 'annotation',
+                'namespace'                    => 'App\\Entity\\',
+                'path'                         => $baseDir. 'src/App/Entity',
+                'use_simple_annotation_reader' => false,
+            ],
+        ],
+    ]
+]);
+
+$app->get('/', function (Application $app) {
+    $foo = new \App\Entity\Foo();
+    $foo->setName('Hello');
+
+    $entityManager = $app['orm.em'];
+
+    $entityManager->persist($foo);
+    $entityManager->flush();
+
+    return new \Symfony\Component\HttpFoundation\Response('Successfully inserted!');
+});
+
 $app->run();
